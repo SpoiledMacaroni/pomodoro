961369bbc1a3ba7df42b5f232f2132bc5a427682
SpoiledMacaroni <60438998+SpoiledMacaroni@users.noreply.github.com>
2024-01-15 13:13:57 -0500

    Add pause and reset functions Refactored code to include pause and reset functionality.

--- pomodoro-app.js
+++ pomodoro-app.js
@@ -1,22 +1,24 @@
 const bells = new Audio('./sounds/bell.wav'); 
 const startBtn = document.querySelector('.btn-start'); 
+const pauseBtn = document.querySelector('.btn-pause');
+const resetBtn = document.querySelector('.btn-reset');
 const session = document.querySelector('.minutes'); 
 let myInterval; 
-let state = true;
+let timerIsOff = true; //is timer off
+let timerIsPaused = false; //is timer paused
+
 
 const appTimer = () => {
   const sessionAmount = Number.parseInt(session.textContent)
 
-  if(state) {
-    state = false;
+  if(timerIsOff) {
+    timerIsOff = false; //timer is on
     let totalSeconds = sessionAmount * 60;
-
+    
     const updateSeconds = () => {
       const minuteDiv = document.querySelector('.minutes');
       const secondDiv = document.querySelector('.seconds');
-    
-      totalSeconds--;
-    
+        
       let minutesLeft = Math.floor(totalSeconds/60);
       let secondsLeft = totalSeconds % 60;
     
@@ -26,10 +28,13 @@ const appTimer = () => {
         secondDiv.textContent = secondsLeft;
       }
       minuteDiv.textContent = `${minutesLeft}`
-    
-      if(minutesLeft === 0 && secondsLeft === 0) {
-        bells.play()
-        clearInterval(myInterval);
+
+      if (!timerIsPaused) { //do something if not paused
+        totalSeconds--;
+        if(minutesLeft === 0 && secondsLeft === 0) {
+          bells.play()
+          clearInterval(myInterval);
+        }
       }
     }
     myInterval = setInterval(updateSeconds, 1000);
@@ -39,3 +44,18 @@ const appTimer = () => {
 }
 
 startBtn.addEventListener('click', appTimer);
+pauseBtn.addEventListener('click', () => {
+  // Toggle the boolean variable
+  timerIsPaused = !timerIsPaused;
+  console.log("Paused!");
+  if (timerIsPaused === true) {pauseBtn.textContent = "Resume"} else {pauseBtn.textContent = "Pause"}});
+resetBtn.addEventListener('click', () => {
+  clearInterval(myInterval); //timer is not ticking
+  timerIsOff = true; //timer is off
+  timerIsPaused = false; //timer is not paused 
+  minuteDiv = document.querySelector('.minutes');
+  minuteDiv.textContent = '25';
+  secondDiv = document.querySelector('.seconds');
+  secondDiv.textContent = '00';
+  if (timerIsPaused === true) {pauseBtn.textContent = "Resume"} else {pauseBtn.textContent = "Pause"}
+});
